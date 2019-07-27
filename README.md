65381df55c245899e0b590cb3520b45ee86d8386
Soda-City-PoGo <sodacitypogo@gmail.com>
2019-07-27 00:09:38 -0400

    stashbot project initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<!-- define variables -->
+[1.1]: http://i.imgur.com/M4fJ65n.png (ATTENTION)
+
+![alt text][1.1] <strong><em>`The contents of this repo are a proof of concept and are for educational use only, all copyrights belong to their respective owners.`</em></strong>![alt text][1.1]<br/>
+
+## This bot requires beginner/intermediate knowledge of discord and bots. This is for a beginner/intermediate user. You will receive beginner/intermediate level support for this.
+
+# Installation:
+## 1: `git clone https://github.com/Soda-City-PoGo/stashbot.git` to desired location or download the zip and unzip.
+
+## 2: cd to the new stashbot folder
+
+## 3: Type `node -v` in your terminal to determine if node is installed on your machine.
+  - If not or version is less than 10.14, upate or download node.js from here: https://nodejs.org/en/
+  - Type `node -v` again in your terminal post install to confirm installation.
+
+## 4: Install npm package requirements.
+  - Run `npm install` in your cloned directory.
+
+## 5: Edit the Config file and save them without the `.example` on them.
+  
+## 6: Start the bot. `pm2 start stashbot.js --watch`
+  - If you get errors that are not because of missing configs, Contact me via discord.
+  - PM2 Docs http://pm2.keymetrics.io/docs/usage/cluster-mode/
---
+++ stashbot.js
@@ -0,0 +1,229 @@
+//
+//		CHECK THE LINE BELOW...DO THESE MAKE NO SENSE? ARE THEY RANDOM CHARACTERS?
+//
+// 		🔴 📵 🗨 📗 🗒 📜 📋 📝 📆 📲 👤 👥 🤖 📥 📤 ✅ ⚠ ⛔ 🚫 ❌ 🔨 🙂 😮 😁 😄 😆 😂 😅 😛 😍 😉 🤔 👍 👎 ❤
+//
+//		THEN YOU NEED TO ADJUST YOUR SETTINGS!!! "Encoding" » "Encode in UTF-8"
+//		... BECAUSE THESE ARE ACTUAL IN-TEXT EMOJIS (WHICH DISCORD ALSO USES)
+//
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const bot=new Discord.Client({fetchAllMembers: true}); //		SLOW LOAD - GET OVER 1B USERS (FROM ALL SERVERS)
+const mysql = require('mysql');
+const winston = require('winston');
+
+
+let debugMode = false;
+for (let val of process.argv){
+    if (val === '-d' || val === '--debug'){
+        debugMode = true;
+        break;
+    }
+}
+
+// Set up logging
+const logger = winston.createLogger({
+    levels: winston.config.syslog.levels,
+    format: winston.format.combine(
+        winston.format.colorize({ all:true }),
+        winston.format.align(),
+        winston.format.timestamp(),
+        winston.format.splat(),  // Allows the text to be formatted at log time
+        winston.format.simple(), // Same as above
+        winston.format.printf(info => '[${info.timestamp}] ${info.level}: ${info.message}')
+    ),
+    transports: [
+        new winston.transports.Console({
+            level: (debugMode ? 'debug' : 'error')
+        })
+    ]
+});
+
+var appexit = (err = null) => {
+    if (err){
+        logger.error(err);
+    }
+    if (Discord){
+        Discord.destroy((err) => {
+            logger.error(err);
+        });
+    }
+    process.exit();
+};
+
+
+//		FUNCTION: TIME STAMP
+//
+function timeStamp(type){
+	let CurrTime=new Date();
+	let mo=CurrTime.getMonth()+1;if(mo<10){mo="0"+mo;}let da=CurrTime.getDate();if(da<10){da="0"+da;}let yr=CurrTime.getFullYear();
+	let hr=CurrTime.getHours();if(hr<10){hr="0"+hr;}let min=CurrTime.getMinutes();if(min<10){min="0"+min;}let sec=CurrTime.getSeconds();if(sec<10){sec="0"+sec;}
+	if(!type || type===0){
+		// [YYYY/MM/DD @ HH:MM:SS]
+		return cc.blue+yr+"/"+mo+"/"+da+" "+hr+":"+min+":"+sec+cc.reset+" |"
+	}
+	if(type===1){
+		// `MM/DD/YYYY` **@** `HH:MM:SS`
+		return "`"+mo+"/"+da+"/"+yr+"` **@** `"+hr+":"+min+":"+sec+"`"
+	}
+	if(type===2){
+		// `MM/DD/YYYY @ HH:MM:SS`
+		return "`"+mo+"/"+da+"/"+yr+" @ "+hr+":"+min+":"+sec+"`"
+	}
+}
+
+
+
+
+// First you need to create a connection to the db
+const con = mysql.createConnection({
+  host  : config.mysqlHost,
+  port  : config.mysqlPort,
+  user  : config.mysqlUser,
+  password  : config.mysqlPass,
+  database  : config.mysqlDB,
+  charset  : "utf8mb4"
+});
+
+con.connect((err) => {
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('🤖stashbot🤖 successfully connected to the RDM database!');
+});
+
+con.end((err) => {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
+client.on('message', message => {
+	console.log(message.content);
+});
+
+client.on('ready', () => {
+	client.user.setStatus("online"); // Set the bot's online/idle/dnd/invisible status
+	//client.user.setPresence({ game: { name: 'my code', type: 'WATCHING'}; // Set the bot's presence (activity and status)
+	console.log("I am logged in on this server as " + client.user.tag)
+    console.log("I am currently running in these servers:")
+    client.guilds.forEach((guild) => {
+    console.log(" - " + guild.name)
+	console.log("total users = "  + client.users.size)
+	console.log("total channels = "+ client.channels.size) 
+	console.log("total servers = " + client.guilds.size);
+	console.log("stashbot is alive, connected to the database, and ready to help you be the very best mapper!");
+	client.user.setActivity('PoGo-Maps', { type: 'Watching' });
+
+        // List all channels
+        //guild.channels.forEach((channel) => {
+           // console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+       // })
+    })
+});
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+}); 
+client.on("message", (message) => {
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(config.prefix + "rdm")) {
+    message.channel.send("https://gitlab.com/realdevicemap/RealDeviceMap-Beta");
+  } else
+  if (message.content.startsWith(config.prefix + "uic")) {
+    message.channel.send("https://gitlab.com/realdevicemap/RealDeviceMap-UIControl-Beta");
+  } else
+  if (message.content.startsWith(config.prefix + "help")) {
+    message.channel.send("**Welcome to stashbot!** \n a bot made by soda to help mappers! \n __Command Prefix__ `!`  \n $#########################################################$ \n **Command List** \n $#########################################################$ \n **rdm** - RealDeviceMap gitlab repo. \n **uic** - RDM UIControl gitlab repo. \n **geojson** - geofence tool. \n **sort** - coordinate sorting tool. \n **wiki** - RDM read the docs. \n **mysql** - latest mysql server download page. \n **homebrew** - Homebrew for MacOS. \n **vmplayer** - Virtual Machine Host for Win/Linux. \n **pmsf** - Frontend for RDM. \n **pokebot** - Alert discord bot for RDM. \n **poracle** - Alert discord bot for RDM. \n **clusters** - Tool for Analyzing spwawnpoints in a RDM database. \n **tools** - Tool for making instances in RDM. \n **monitor** - discord bot to monitor RDM. \n **spoofninja** - discord bot to monitor for spoofers. \n **force** - discord bot to monitor current pogo version. \n **build** - command to build RDM UIC. \n **blocktrade** - Site to trade BTC for XMR. \n **yml** - example .yml for RDM docker image. \n **docker** - helpful docker commands. \n **gds** - :sunglasses: :iphone: \n **xmr** - current price of XMR. \n **stats** - stat page for RDM. \n **icons** - icon repos for pmsf/pokebot/RDM. \n **nest** - script for finding nest in RDM. \n **manmr** - commands to pull a merge request for rdm-uic. \n **help** - show this message.");	
+  } else
+  if (message.content.startsWith(config.prefix + "geojson")) {
+    message.channel.send("http://geojson.io/#map=2/20.0/0.0");
+  } else
+  if (message.content.startsWith(config.prefix + "sort")) {
+    message.channel.send("https://github.com/Kneckter/AutoSortCoords");
+  } else
+  if (message.content.startsWith(config.prefix + "wiki")) {
+    message.channel.send("https://realdevicemap.readthedocs.io/en/latest/index.html");
+  } else
+  if (message.content.startsWith(config.prefix + "mysql")) {
+    message.channel.send("https://dev.mysql.com/downloads/mysql/");
+  } else
+  if (message.content.startsWith(config.prefix + "homebrew")) {
+    message.channel.send("https://brew.sh/");
+  } else
+  if (message.content.startsWith(config.prefix + "vmplayer")) {
+    message.channel.send("https://my.vmware.com/en/web/vmware/free#desktop_end_user_computing/vmware_workstation_player/15_0");	
+  }	else
+  if (message.content.startsWith(config.prefix + "pmsf")) {
+    message.channel.send("https://github.com/whitewillem/PMSF");
+  } else
+  if (message.content.startsWith(config.prefix + "pokebot")) {
+    message.channel.send("https://gitlab.com/webhookdatareceiver/Webhook-Data-Receiver");
+  } else
+  if (message.content.startsWith(config.prefix + "poracle")) {
+    message.channel.send("https://github.com/KartulUdus/PoracleJS");
+  } else
+  if (message.content.startsWith(config.prefix + "clusters")) {
+    message.channel.send("https://github.com/Kneckter/SpawnpointClusterTool");
+  } else
+  if (message.content.startsWith(config.prefix + "tools")) {
+    message.channel.send("https://github.com/abakedapplepie/RealDeviceMap-tools");
+  } else
+  if (message.content.startsWith(config.prefix + "monitor")) {
+    message.channel.send("https://github.com/chuckleslove/RDMMonitor");
+  } else
+  if (message.content.startsWith(config.prefix + "spoofninja")) {
+    message.channel.send("https://github.com/JennerPalacios/SimpleSpoofNinja");
+  } else
+  if (message.content.startsWith(config.prefix + "force")) {
+    message.channel.send("https://github.com/chuckleslove/DiscordPogoVersionMonitor");
+  } else
+  if (message.content.startsWith(config.prefix + "build")) {
+    message.channel.send("`swift build && cp ./.build/x86_64-apple-macosx10.10/debug/RDM-UIC-Manager .`");
+  } else
+  if (message.content.startsWith(config.prefix + "blocktrade")) {
+    message.channel.send("https://blocktrades.us/");
+  } else
+  if (message.content.startsWith(config.prefix + "yml")) {
+    message.channel.send("https://pastebin.com/raw/Sf9JTRQy");
+  } else
+  if (message.content.startsWith(config.prefix + "manmr")) {
+    message.channel.send("`git fetch origin merge-requests/72/head:master` \n `git checkout master` \n `cd Manager` \n `swift build && cp ./.build/x86_64-apple-macosx10.10/debug/RDM-UIC-Manager .` \n `./RDM-UIC-Manager` \n `git fetch origin merge-requests/71/head:invasion_spin_close` \n `git checkout invasion_spin_close` \n `cd Manager` \n `swift build && cp ./.build/x86_64-apple-macosx10.10/debug/RDM-UIC-Manager .` \n `./RDM-UIC-Manager`");	
+  } else
+  if (message.content.startsWith(config.prefix + "docker")) {
+    message.channel.send("**Useful Docker Commands** \n `sudo docker ps -a -s` - Show all containers \n `sudo docker system df` - Show docker disk usage \n `sudo docker image ls -a` - Show all images \n `sudo docker logs -f contianerID | grep keyword` - \n Search docker logs for a specific keyword or phrase, \n **Note** keyword or phrase needs to be surrounded in double quotes! \n `sudo docker system prune -a` - a single command that will clean up all docker resources!");	
+  } else
+  if (message.content.startsWith(config.prefix + "gds")) {
+    message.channel.send("`GDS 1.3 version, Pokemon GO (0.149.0-A)` \n https://mega.nz/#!e7xwnAzQ!FqQGsMxT2l-pPKE2hKyzDci86hAnjK9UUcZQWcheuNo ");	
+  } else
+  if (message.content.startsWith(config.prefix + "xmr")) {
+    message.channel.send("https://www.coinbase.com/price/monero");
+  } else
+  if (message.content.startsWith(config.prefix + "stats")) {
+    message.channel.send("https://github.com/versx/RealDeviceMap-opole");	
+  } else
+  if (message.content.startsWith(config.prefix + "icons")) {
+    message.channel.send("`https://raw.githubusercontent.com/nileplumb/PkmnShuffleMap/master/PMSF_icons_large/` \n `https://raw.githubusercontent.com/Aranoh/pkmn_shuffle_icons_pokesquad/master/optimized_for_PMSF_frontend/` \n `https://raw.githubusercontent.com/geekygreek7/pkmn_shuffle_icons/master/optimized_for_PMSF_frontend/` \n `https://raw.githubusercontent.com/shindekokoro/PkmnShuffleMap/master/PMSF_icons_large/` \n `https://raw.githubusercontent.com/tallypokemap/derpysprites/master/`");
+  } else
+  if (message.content.startsWith(config.prefix + "nest")) {
+    message.channel.send("https://github.com/M4d40/PMSFnestScript");
+  } else 
+  if (message.content.startsWith(config.prefix + "server")) {
+	message.channel.send("Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}");
+}
+});
+
+//do some cleanup on ctrl + c
+process.on('SIGINT', () => {
+    logger.debug('CTRL + C pressed - shutting down');
+    appexit();
+});
+
+
+ 
+client.login(config.token);
